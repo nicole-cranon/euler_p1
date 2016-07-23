763897dde8a8f2934dcb328453334e88413353b9
nicole-cranon <nicole.cranon@gmail.com>
2016-07-23 10:44:14 -0600

    change mltp3And5 to mulps for general use

--- js/index.js
+++ js/index.js
@@ -6,13 +6,13 @@ var valuesToBeDivisibleBy = [3,5];			// the values to be divisible by are 3 OR 5
 var rangeUpperNonInclusiveEnd = 1000;		// the range of values to be checked for divisibility by 3 OR 5 are
 											// in [0,1000). that is all non-negative integers below 1000
 											// divisible by 3 OR 5 will be summed
-var mltp3And5 = getMultiplesOfInRange(valuesToBeDivisibleBy, rangeUpperNonInclusiveEnd);	// the array of values under 1000
+var mltps = getMultiplesOfInRange(valuesToBeDivisibleBy, rangeUpperNonInclusiveEnd);	// the array of values under 1000
 																							// divisible by 3 OR 5
 
 // find sum of multiples
-sumOfMultiples = _.reduce(mltp3And5, function (sum, n) {
+sumOfMultiples = _.reduce(mltps, function (sum, n) {
 	return sum + n;
 }, 0);
 
-console.log ("Multiples of {" + valuesToBeDivisibleBy + "} in the range [0," + rangeUpperNonInclusiveEnd + "): " + mltp3And5);
+console.log ("Multiples of {" + valuesToBeDivisibleBy + "} in the range [0," + rangeUpperNonInclusiveEnd + "): " + mltps);
 console.log ("Sum of multiples: " + sumOfMultiples);
